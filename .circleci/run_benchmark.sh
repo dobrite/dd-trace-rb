ef90efa0468c15f00fbb183a3ae2574166c37313
Pawel Chojnacki <pawel.chcki@gmail.com>
2018-08-17 14:56:51 +0200

    WIP3

--- .circleci/run_benchmark.sh
+++ .circleci/run_benchmark.sh
@@ -5,7 +5,7 @@ TEST_NAME=$2
 TEST_FILE=benchmarks/$TEST_NAME.rb
 
 RESULT_DIR=$STORAGE_DIR/$TEST_NAME
-RESULT_FILE=$RESULT_DIR/$TEST_NAME/benchmark-$CIRCLE_BUILD_NUM-$CIRCLE_NODE_INDEX.csv
+RESULT_FILE=$RESULT_DIR/benchmark-$CIRCLE_BUILD_NUM-$CIRCLE_NODE_INDEX.csv
 mkdir -p $RESULT_DIR
 
 bundle exec appraisal rails5-postgres-sidekiq ruby $TEST_FILE 2>&1 1>/dev/null | tee $RESULT_FILE
